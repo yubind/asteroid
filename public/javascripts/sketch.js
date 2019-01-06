ef895c7fb82e6fbf55d2684abb0b3de3b2d579e9
Yubin Kim <yubind91@gmail.com>
2019-01-06 21:50:29 +0000

    asteroid setup

---
+++ public/javascripts/sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+    background(0);
+}
+
+function Ship() {
+    this.pos = createCanvas(width/2, height/2);
+}
