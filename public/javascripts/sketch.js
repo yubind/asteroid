e65a430498b0b50fca9686344173b1f6c0463f52
Yubin Kim <yubind91@gmail.com>
2019-01-06 22:17:48 +0000

    add ship

--- public/javascripts/sketch.js
+++ public/javascripts/sketch.js
@@ -1,11 +1,21 @@
+var ship;
+
 function setup() {
     createCanvas(windowWidth, windowHeight);
+    ship = new Ship();
 }
 
 function draw() {
     background(0);
+    ship.render();
 }
 
 function Ship() {
-    this.pos = createCanvas(width/2, height/2);
+    this.pos = createVector(width/2, height/2);
+    this.r = 10;
+    this.render = function() {
+        translate(this.pos.x, this.pos.y);
+        triangle(-this.r, this.r, this.r, this.r, 0, -this.r);
+    }
+    
 }
