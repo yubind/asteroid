c5f06744f7f863d015434855ea695885c54ab5ab
Yubin Kim <yubind91@gmail.com>
2019-01-07 05:25:15 +0000

    add keypress functions

--- public/javascripts/sketch.js
+++ public/javascripts/sketch.js
@@ -8,14 +8,46 @@ function setup() {
 function draw() {
     background(0);
     ship.render();
+    ship.turn();
+    ship.update();
+}
+
+function keyReleased() {
+    ship.setRotation(0);
+}
+
+function keyPressed() {
+    if(keyCode == RIGHT_ARROW) {
+        ship.setRotation(0.1);
+    } else if(keyCode == LEFT_ARROW) {
+        ship.setRotation(-0.1);
+    }
 }
 
 function Ship() {
-    this.pos = createVector(width/2, height/2);
-    this.r = 10;
+    this.pos = createVector(width / 2, height / 2);
+    this.r = 20;
+    this.heading = 0.1;
+    this.rotation = 0;
+    this.vel = createVector(0, 0);
+    
+    this.update = function() {
+        this.pos.add(this.vel);
+    }
+    
     this.render = function() {
         translate(this.pos.x, this.pos.y);
+        rotate(this.heading);
+        noFill();
+        stroke(255);
         triangle(-this.r, this.r, this.r, this.r, 0, -this.r);
     }
     
+    this.setRotation = function(a) {
+        this.rotation = a;
+    }
+    
+    this.turn = function() {
+        this.heading += this.rotation;
+    }
 }
